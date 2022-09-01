f8e88b799e5ab8fb05bf1b134bdc761e813dda8c
kunalkeshan <kunalkeshan12@gmail.com>
2022-09-01 08:13:24 +0530

    adding home pages

---
+++ src/pages/Home/Contribute.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Contribute = () => {
+	return <div>Contribute</div>;
+};
+
+export default Contribute;
---
+++ src/pages/Home/Issues.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Issues = () => {
+	return <div>Issues</div>;
+};
+
+export default Issues;
