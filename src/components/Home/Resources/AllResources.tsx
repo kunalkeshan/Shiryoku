d6a6bd1072db01e6cbaa8a41f04f720676e36e17
kunalkeshan <kunalkeshan12@gmail.com>
2022-09-02 00:48:44 +0530

    setting up files for resources page

---
+++ src/components/Home/Resources/AllResources.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AllResources = () => {
+	return <div>AllResources</div>;
+};
+
+export default AllResources;
---
+++ src/components/Home/Resources/Tags.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Tags = () => {
+	return <div>Tags</div>;
+};
+
+export default Tags;
