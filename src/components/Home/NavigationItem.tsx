e71a5706d1ee3adef9276031076bf1dcfd0f062f
kunalkeshan <kunalkeshan12@gmail.com>
2022-09-01 11:41:49 +0530

    home page sidebar configured

---
+++ src/components/Home/NavigationItem.tsx
@@ -0,0 +1,84 @@
+/**
+ * Navigation Item
+ */
+
+import React, { FC, PropsWithChildren } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import {
+	ListItem,
+	ListItemButton,
+	ListItemIcon,
+	ListItemText,
+	styled,
+} from '@mui/material';
+
+interface NavigationItemProps extends PropsWithChildren {
+	name: string;
+	nav?: string;
+	link?: string;
+	icon: any;
+	currentPage: string;
+	open: boolean;
+}
+
+const NavigationItem: FC<NavigationItemProps> = (props) => {
+	const navigate = useNavigate();
+
+	const buttonStyling =
+		props?.nav && props?.nav === props.currentPage
+			? {
+					justifyContent: props.open ? 'initial' : 'center',
+					backgroundColor: '#90caf9',
+			  }
+			: {
+					justifyContent: props.open ? 'initial' : 'center',
+			  };
+
+	const iconStyling =
+		props?.nav && props?.nav === props.currentPage
+			? {
+					mr: props.open ? 3 : 'auto',
+					// TODO: Add icon styling later
+			  }
+			: {
+					mr: props.open ? 3 : 'auto',
+			  };
+
+	const handleNavigate = ({ link, nav }: { link?: string; nav?: string }) => {
+		if (link) {
+			window.open(link, '_blank');
+		} else {
+			navigate(nav || '');
+		}
+	};
+
+	return (
+		<ListItem key={props.name} disablePadding sx={{ display: 'block' }}>
+			<Button
+				sx={buttonStyling}
+				onClick={() =>
+					handleNavigate({ link: props?.link, nav: props?.nav })
+				}
+			>
+				<Icon sx={iconStyling}>{props.icon}</Icon>
+				<ListItemText
+					primary={props.name}
+					sx={{ opacity: props.open ? 1 : 0 }}
+				/>
+			</Button>
+		</ListItem>
+	);
+};
+
+const Button = styled(ListItemButton)({
+	minHeight: 48,
+	px: 2.5,
+});
+
+const Icon = styled(ListItemIcon)({
+	minWidth: 0,
+	justifyContent: 'center',
+});
+
+export default NavigationItem;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,16 +3,226 @@
  */
 
 // Dependencies
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
+import { styled, useTheme, Theme, CSSObject } from '@mui/material/styles';
+import {
+	Box,
+	Toolbar,
+	List,
+	CssBaseline,
+	Typography,
+	Divider,
+	IconButton,
+} from '@mui/material';
+import MuiDrawer from '@mui/material/Drawer';
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+
+import CategoryIcon from '@mui/icons-material/Category';
+import AppsIcon from '@mui/icons-material/Apps';
+import PersonAddAlt1Icon from '@mui/icons-material/PersonAddAlt1';
+import BugReportIcon from '@mui/icons-material/BugReport';
+import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
+import VolunteerActivismIcon from '@mui/icons-material/VolunteerActivism';
+import GitHubIcon from '@mui/icons-material/GitHub';
+
+import NavigationItem from '../../components/Home/NavigationItem';
+
+import config from '../../config';
+
+const drawerWidth = 240;
 
 const Home = () => {
+	const location = useLocation();
+	const theme = useTheme();
+	const [open, setOpen] = useState(false);
+	const [currentPage, setCurrentPage] = useState('');
+
+	const MAIN_NAV_ITEMS = [
+		{
+			name: 'Resources',
+			nav: '/',
+			icon: <AppsIcon />,
+		},
+		{
+			name: 'Contribute',
+			nav: '/contribute',
+			icon: <PersonAddAlt1Icon />,
+		},
+		{
+			name: 'Issues',
+			nav: '/issues',
+			icon: <BugReportIcon />,
+		},
+		{
+			name: 'Contact',
+			nav: '/contact',
+			icon: <AlternateEmailIcon />,
+		},
+		{
+			name: 'Support',
+			nav: '/support',
+			icon: <VolunteerActivismIcon />,
+		},
+	];
+
+	const ADDITIONAL_NAV_ITEMS = [
+		{
+			name: 'Repo',
+			link: config.GITHUB_URL,
+			icon: <GitHubIcon />,
+		},
+	];
+
+	const handleDrawerOpen = () => {
+		setOpen(true);
+	};
+
+	const handleDrawerClose = () => {
+		setOpen(false);
+	};
+
+	useEffect(() => {
+		const currentLocation = location.pathname;
+		setCurrentPage(currentLocation);
+	}, [location]);
+
 	return (
-		<div>
-			Home
-			<Outlet />
-		</div>
+		<Box sx={{ display: 'flex' }}>
+			<CssBaseline />
+			<AppBar position='fixed' open={open}>
+				<Toolbar>
+					<IconButton
+						color='inherit'
+						aria-label='open drawer'
+						onClick={handleDrawerOpen}
+						edge='start'
+						sx={{
+							marginRight: 5,
+							...(open && { display: 'none' }),
+						}}
+					>
+						<MenuIcon />
+					</IconButton>
+					<Typography variant='h6' noWrap component='div'>
+						Resources{' '}
+						<Typography variant='caption' component='span'>
+							by Kunal Keshan
+						</Typography>
+					</Typography>
+				</Toolbar>
+			</AppBar>
+			<Drawer variant='permanent' open={open}>
+				<DrawerHeader>
+					<IconButton onClick={handleDrawerClose}>
+						{theme.direction === 'rtl' ? (
+							<ChevronRightIcon />
+						) : (
+							<ChevronLeftIcon />
+						)}
+					</IconButton>
+				</DrawerHeader>
+				<Divider />
+				<List>
+					{MAIN_NAV_ITEMS.map((item, index) => (
+						<NavigationItem
+							{...item}
+							key={index}
+							open={open}
+							currentPage={currentPage}
+						/>
+					))}
+				</List>
+				<Divider />
+				<List>
+					{ADDITIONAL_NAV_ITEMS.map((item, index) => (
+						<NavigationItem
+							{...item}
+							key={index}
+							open={open}
+							currentPage={currentPage}
+						/>
+					))}
+				</List>
+			</Drawer>
+			<Box component='main' sx={{ flexGrow: 1, p: 3 }}>
+				<DrawerHeader />
+				<Outlet />
+			</Box>
+		</Box>
 	);
 };
+
+const openedMixin = (theme: Theme): CSSObject => ({
+	width: drawerWidth,
+	transition: theme.transitions.create('width', {
+		easing: theme.transitions.easing.sharp,
+		duration: theme.transitions.duration.enteringScreen,
+	}),
+	overflowX: 'hidden',
+});
+
+const closedMixin = (theme: Theme): CSSObject => ({
+	transition: theme.transitions.create('width', {
+		easing: theme.transitions.easing.sharp,
+		duration: theme.transitions.duration.leavingScreen,
+	}),
+	overflowX: 'hidden',
+	width: `calc(${theme.spacing(7)} + 1px)`,
+	[theme.breakpoints.up('sm')]: {
+		width: `calc(${theme.spacing(8)} + 1px)`,
+	},
+});
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+	display: 'flex',
+	alignItems: 'center',
+	justifyContent: 'flex-end',
+	padding: theme.spacing(0, 1),
+	// necessary for content to be below app bar
+	...theme.mixins.toolbar,
+}));
+
+interface AppBarProps extends MuiAppBarProps {
+	open?: boolean;
+}
+
+const AppBar = styled(MuiAppBar, {
+	shouldForwardProp: (prop) => prop !== 'open',
+})<AppBarProps>(({ theme, open }) => ({
+	zIndex: theme.zIndex.drawer + 1,
+	transition: theme.transitions.create(['width', 'margin'], {
+		easing: theme.transitions.easing.sharp,
+		duration: theme.transitions.duration.leavingScreen,
+	}),
+	...(open && {
+		marginLeft: drawerWidth,
+		width: `calc(100% - ${drawerWidth}px)`,
+		transition: theme.transitions.create(['width', 'margin'], {
+			easing: theme.transitions.easing.sharp,
+			duration: theme.transitions.duration.enteringScreen,
+		}),
+	}),
+}));
+
+const Drawer = styled(MuiDrawer, {
+	shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+	width: drawerWidth,
+	flexShrink: 0,
+	whiteSpace: 'nowrap',
+	boxSizing: 'border-box',
+	...(open && {
+		...openedMixin(theme),
+		'& .MuiDrawer-paper': openedMixin(theme),
+	}),
+	...(!open && {
+		...closedMixin(theme),
+		'& .MuiDrawer-paper': closedMixin(theme),
+	}),
+}));
 
 export default Home;
