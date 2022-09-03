dc5cadae429e6856d00360231577bef5d8ec35bb
kunalkeshan <kunalkeshan12@gmail.com>
2022-09-03 23:22:26 +0530

    about page styling

--- src/pages/Home/About.tsx
+++ src/pages/Home/About.tsx
@@ -5,7 +5,7 @@
 // Dependencies
 import React from 'react';
 
-import { Typography, styled, Box, Divider } from '@mui/material';
+import { Typography, styled, Box, Divider, Grid, Paper } from '@mui/material';
 
 const About = () => {
 	return (
@@ -17,10 +17,48 @@ const About = () => {
 				</Typography>
 			</Typography>
 			<Divider />
+			<TranslationBox elevation={3}>
+				<Grid container id='container'>
+					<Grid item xs={5} className='container__box'>
+						<Typography variant='h5'>資</Typography>
+						<p>Japanese</p>
+					</Grid>
+					<Grid item xs={7} className='container__box'>
+						<Typography variant='h5'>Resources</Typography>
+						<p>English</p>
+					</Grid>
+				</Grid>
+			</TranslationBox>
 		</Main>
 	);
 };
 
 const Main = styled(Box)({});
+
+const TranslationBox = styled(Paper)({
+	padding: '1em',
+	width: 'clamp(200px, 100%, 600px)',
+	margin: '1em auto',
+	userSelect: 'none',
+	'#container': {
+		backgroundColor: '#80808030',
+		borderRadius: '4px',
+		'.container__box': {
+			padding: '1em',
+			textAlign: 'center',
+			fontSize: '1em',
+			p: {
+				fontSize: '0.8rem',
+			},
+			border: 'solid 1px #80808060',
+			':first-child': {
+				borderRadius: '4px 0 0 4px',
+			},
+			':last-child': {
+				borderRadius: '0 4px 4px 0',
+			},
+		},
+	},
+});
 
 export default About;
