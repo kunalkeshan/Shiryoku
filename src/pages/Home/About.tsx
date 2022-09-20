aad946d4fe50da48555fed6174fad2fff285e3b2
kunalkeshan <kunalkeshan12@gmail.com>
2022-09-20 17:02:33 +0530

    about page added

--- src/pages/Home/About.tsx
+++ src/pages/Home/About.tsx
@@ -5,7 +5,44 @@
 // Dependencies
 import React from 'react';
 
-import { Typography, styled, Box, Divider, Grid, Paper } from '@mui/material';
+import {
+	Typography,
+	styled,
+	Box,
+	Divider,
+	Grid,
+	Paper,
+	Container,
+} from '@mui/material';
+
+import TagsGif from '../../assets/howToUse/tags.gif';
+import SearchGif from '../../assets/howToUse/search.gif';
+import ShareGif from '../../assets/howToUse/share.gif';
+import RoadmapsGif from '../../assets/howToUse/roadmaps.gif';
+import { info } from 'console';
+
+const data = [
+	{
+		image: TagsGif,
+		label: 'Sort resources using Tags',
+		text: 'Use tags to sort the specific resources that you are looking for. When needed, use the "Clear Tags" button to clear all selected tags.',
+	},
+	{
+		image: SearchGif,
+		label: 'Search for Resources',
+		text: "Search for resources based on the name, description and tags associated to it. Clear the search when you're done searching.",
+	},
+	{
+		image: ShareGif,
+		label: 'Share and visit resource',
+		text: 'Use the "Copy the URL to your clipboard" to share the URL directly, or visit the website, the GitHub repo (if open source) and share it optionally where you need to.',
+	},
+	{
+		image: RoadmapsGif,
+		label: 'Use Roadmaps',
+		text: 'Roadmaps is an organized approach to Resources. Share the roadmap with others, and use it to follow a structured learning path.',
+	},
+];
 
 const About = () => {
 	return (
@@ -29,6 +66,38 @@ const About = () => {
 					</Grid>
 				</Grid>
 			</TranslationBox>
+			<Container>
+				{data.map((info, index) => (
+					<Box
+						key={index}
+						sx={{
+							display: 'flex',
+							alignItems: 'center',
+							mt: 4,
+							flexDirection:
+								index % 2 === 0 ? 'row' : 'row-reverse',
+							gap: '1em',
+							'@media (max-width: 800px)': {
+								flexWrap: 'wrap',
+								flexDirection: 'row',
+							},
+						}}
+					>
+						<Box>
+							<Typography variant='h4'>{info.label}</Typography>
+							<Typography variant='h6'>{info.text}</Typography>
+						</Box>
+						<img
+							src={info.image}
+							alt={info.label}
+							style={{
+								width: 'min(100%, 400px)',
+								height: 'auto',
+							}}
+						/>
+					</Box>
+				))}
+			</Container>
 		</Main>
 	);
 };
