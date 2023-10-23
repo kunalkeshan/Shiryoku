2d63edc790a5cf63d70f818b9c69a890267c09fd
Ahad1713 <khanazan761@gmail.com>
2023-10-24 01:05:43 +0530

    Add next.js roadmap

--- src/data/roadmaps.ts
+++ src/data/roadmaps.ts
@@ -15,6 +15,21 @@ const ROADMAPS = [
 			{ id: '001-4', name: 'Tools' },
 		],
 	},
+	{
+		id: '002',
+		name: 'Introduction to Next.js',
+		description:
+			'Next.js: The React Framework for Building Fast, Scalable, and Server-Rendered Web Applications.',
+		icon: './assets/roadmaps/002/icon.png',
+		cover: './assets/roadmaps/002/cover.png',
+		topics: [
+			{ id: '002-0', name: 'What is Next.js?' },
+			{ id: '002-1', name: 'Getting Started with Next.js' },
+			{ id: '002-2', name: 'Server-Side Rendering (SSR) in Next.js' },
+			{ id: '002-3', name: 'Client-Side Routing with Next.js' },
+			{ id: '002-4', name: 'Data Fetching in Next.js' },
+		],
+	},
 ];
 
 export default ROADMAPS;
