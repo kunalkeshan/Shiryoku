490f288db7d51669b0311d7af4ac693b9dc207d9
Disha <dishay166@gmail.com>
2023-10-24 01:30:02 +0530

    feat:add reactjs resources

--- src/data/roadmaps.ts
+++ src/data/roadmaps.ts
@@ -15,6 +15,26 @@ const ROADMAPS = [
 			{ id: '001-4', name: 'Tools' },
 		],
 	},
+	{
+		id: '002',
+		name: 'Roadmap to master ReactJs',
+		description:
+			'Ways and assets that can enhance your efficiency in software development.',
+		icon: './assets/roadmaps/002/icon.png',
+		cover: './assets/roadmaps/002/cover.png',
+		topics: [
+			{ id: '002-0', name: 'React Official Documentation' },
+			{ id: '002-1', name: 'Create React App' },
+			{ id: '002-2', name: 'React Router' },
+			{ id: '002-3', name: 'React Hooks' },
+			{ id: '002-4', name: 'Redux' },
+			{ id: '002-5', name: 'Advanced Topics and Best Practices' },
+			{ id: '002-6', name: 'Testing in React' },
+			{ id: '002-7', name: 'Performance Optimization' },
+			{ id: '002-8', name: 'Real-world Projects and Tutorials' },
+			{ id: '002-9', name: 'Community and Forums' },
+		],
+	},
 ];
 
 export default ROADMAPS;
